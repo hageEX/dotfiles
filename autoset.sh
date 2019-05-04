9fe641f0916554f0c7058e813026b3505208a95a
hageEX <gangangandon@icloud.com>
2019-05-04 10:57:43 +0900

    Update

--- autoset.sh
+++ autoset.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 
 if [ ! -f ~/.vim/autoload/plug.vim ] ; then
-    echo 'Install... [vim-plug.vim]'
+    echo 'Install... [vim-plug]'
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-else
-    :
 fi
 
 if [ ! -f ~/.vim/colors/despacio.vim ] ; then
     echo 'Install... [despacio.vim]'
     curl -fLo ~/.vim/colors/despacio.vim --create-dirs \
         https://raw.githubusercontent.com/AlessandroYorba/Despacio/master/colors/despacio.vim
-else
-    :
 fi
 
 echo 'Add... [.vimrc]\n'
 echo 'source ~/dotfiles/dot.vimrc' >> ~/.vimrc
+echo 'Add... [.zshrc]\n'
+echo 'source ~/dotfiles/dot.zshrc' >> ~/.zshrc
 
 echo '__Finally__'
 echo '$ vim'
