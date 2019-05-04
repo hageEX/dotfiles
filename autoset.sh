5ca077b0441fff3e825a097698105e1e96832d3d
hageEX <gangangandon@icloud.com>
2019-05-04 09:46:29 +0900

    Update

--- autoset.sh
+++ autoset.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f ~/.vim/autoload/plug.vim ] ; then
     echo 'Install... [vim-plug.vim]'
-    ~/.vim/autoload/plug.vim --create-dirs \
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 else
     :
@@ -10,9 +10,8 @@ fi
 
 if [ ! -f ~/.vim/colors/despacio.vim ] ; then
     echo 'Install... [despacio.vim]'
-    mkdir ~/.vim/colors
-    cd ~/.vim/colors
-    curl https://raw.githubusercontent.com/AlessandroYorba/Despacio/master/colors/despacio.vim > despacio.vim
+    curl -fLo ~/.vim/colors/despacio.vim --create-dirs \
+        https://raw.githubusercontent.com/AlessandroYorba/Despacio/master/colors/despacio.vim
 else
     :
 fi
